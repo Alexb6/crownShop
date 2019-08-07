d94cc89a7767e3005964c80cc256bce699559d99
Alex Tran <info@alex-tran.com>
2019-08-07 12:48:24 +0200

    added the withRouter HOC for the menu-item and set the first routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/home/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = (props) => {
+	console.log(props);
+	
+	return (
+		<div>
+			<h1>Hats Page</h1>
+		</div>
+	);
+}
+
+// function App() {
+// 	return (
+// 		<div>
+// 			<HomePage />
+// 		</div>
+// 	);
+// }
 
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route exact path='/hats' component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
