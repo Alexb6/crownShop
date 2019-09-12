fe0f4a45e0e8cea8382613c6a19bca3a0b49505a
Alex Tran <info@alex-tran.com>
2019-09-12 13:50:03 +0200

    solved another the error

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import rootReducer from './root-reducer';
 const middlewares = [];
 
 // Apply Middlewares only in develpment environnement and not in build or test
-if (process.env.NODE.ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
